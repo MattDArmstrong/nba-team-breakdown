45fd092ef4d8062c795e726ce7ac398e75acf6fd
MattDArmstrong <matt.d.a@shaw.ca>
2021-02-10 14:31:44 -0700

    Commit to add css modules

---
+++ src/componenets/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Header.css'
+
+const header = () => {
+    return(
+        <div className={classes.Header}>NBA Team Breakdown</div> 
+    );
+}
+
+export default header;
